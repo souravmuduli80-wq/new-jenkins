ca6259c16f4489144b671239ec357fcb8dbdcd2c
souravmuduli80-wq <souravmuduli80@gmail.com>
2026-05-25 11:51:20 +0530

    first commit

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+
+    public static void main(String[] args) {
+
+        System.out.println("Student Application Started");
+
+        String name = "Gokul";
+        int age = 22;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age  : " + age);
+
+        System.out.println("Student Application Completed");
+    }
+}
